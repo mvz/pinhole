c77af2c7b93e7ae7c139c7d61faad5232e51a567
Matijs van Zuijlen <Matijs.van.Zuijlen@xs4all.nl>
2008-12-28 20:01:49 +0100

    Make viewer into a class

--- viewer.rb
+++ viewer.rb
@@ -1,13 +1,26 @@
 require 'gtk2'
 
-Gtk.init
+class Viewer
+  def initialize(filename)
+    @filename = filename
+  end
 
-window = Gtk::Window.new
-buf = Gdk::Pixbuf.new("_DSC2502.jpg", 1024, 768)
-im = Gtk::Image.new(buf)
-eb = Gtk::EventBox.new.add(im)
-window.signal_connect("destroy") { Gtk.main_quit }
-window.border_width = 10
-window.add(eb)
-window.show_all
-Gtk.main
+  def run
+    Gtk.init
+
+    window = Gtk::Window.new
+    buf = Gdk::Pixbuf.new(@filename, 1024, 768)
+    im = Gtk::Image.new(buf)
+    eb = Gtk::EventBox.new.add(im)
+    window.signal_connect("destroy") { Gtk.main_quit }
+    window.border_width = 0
+    window.add(eb)
+    window.show_all
+    Gtk.main
+  end
+end
+
+
+viewer = Viewer.new("_DSC2502.jpg")
+
+viewer.run
