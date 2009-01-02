a151da7156074d3454f60509ccf77e1c041f1ae1
Matijs van Zuijlen <Matijs.van.Zuijlen@xs4all.nl>
2009-01-02 07:38:27 +0100

    Drag the image around

--- Rakefile
+++ Rakefile
@@ -1,11 +1,14 @@
+require 'rake'
+desc "Build and run"
 task :default => [:interface, :test]
 
+desc "Build interface"
 task :interface do
   `gtk-builder-convert rthumb.glade rthumb.xml`
 end
 
+desc "Perform test run"
 task :test do
   #`ruby viewer.rb _DSC2502.jpg`
   `ruby viewer.rb p1013366.jpg`
 end
-
--- viewer.rb
+++ viewer.rb
@@ -29,7 +29,7 @@ class Viewer
     @builder.add "rthumb.xml"
     @builder.connect_signals { |name| method(name) }
 
-    @builder["viewport1"].set_size_request(0,0)
+    @builder["viewport"].set_size_request(0,0)
   end
 
   def on_mainwindow_destroy
@@ -40,11 +40,11 @@ class Viewer
     if e.new_window_state.fullscreen?
       @builder["menubar"].visible = false
       @builder["statusbar"].visible = false
-      @builder["scrolledwindow1"].set_policy(:never, :never)
+      @builder["scrolledwindow"].set_policy(:never, :never)
     else
       @builder["menubar"].visible = true
       @builder["statusbar"].visible = true
-      @builder["scrolledwindow1"].set_policy(:automatic, :automatic)
+      @builder["scrolledwindow"].set_policy(:automatic, :automatic)
     end
   end
 
@@ -56,6 +56,27 @@ class Viewer
       @fullscreen = true
       @window.fullscreen
     end
+  end
+
+  def on_viewport_button_press_event w, e
+    @dragging = true
+    @dragx = e.x_root
+    @dragy = e.y_root
+    @scrollx = @builder["viewport"].hadjustment.value
+    @scrolly = @builder["viewport"].vadjustment.value
+  end
+
+  def on_viewport_button_release_event w, e
+    @dragging = false
+  end
+
+  def on_viewport_motion_notify_event w, e
+    return false unless @dragging
+    dx = e.x_root - @dragx
+    dy = e.y_root - @dragy
+
+    @builder["viewport"].hadjustment.value = @scrollx - dx
+    @builder["viewport"].vadjustment.value = @scrolly - dy
   end
 end
 
