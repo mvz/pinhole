02c90eaf6653baccb0069d8ee21c8bd019f6f88e
Matijs van Zuijlen <Matijs.van.Zuijlen@xs4all.nl>
2009-01-13 19:56:53 +0100

    Extract class Browser

--- browser.rb
+++ browser.rb
@@ -1,17 +1,35 @@
 require 'optparse'
 require 'gtk2'
 
+module Pinhole
+  class Browser < Gtk::ScrolledWindow
+    def initialize
+      super
+      @iconview = Gtk::IconView.new
+      self.add @iconview
+      self.set_policy(:automatic, :automatic)
+      @iconview.set_size_request 0, 0
+      @iconview.text_column = 0
+      @iconview.pixbuf_column = 1
+    end
+
+    def model= m
+      @iconview.model = m
+    end
+  end
+end
+
 class Browser
   def run
     Gtk.init
 
     setup_ui
 
-    @fullscreen = false
-
     @window = @builder["mainwindow"]
 
-    @iconview = @builder["iconview"]
+    @browser = Pinhole::Browser.new
+
+    @builder["mainvbox"].pack_start(@browser)
 
     @store = Gtk::ListStore.new(String, Gdk::Pixbuf)
 
@@ -20,11 +38,7 @@ class Browser
       @store.append.set_value(0, f).set_value(1, pb)
     }
 
-    @iconview.set_size_request(0,0)
-
-    @iconview.model = @store
-    @iconview.text_column = 0
-    @iconview.pixbuf_column = 1
+    @browser.model = @store
 
     @window.show_all
 
