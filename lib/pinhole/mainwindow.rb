df7852d79e27a0af8fa56f423e53094b4cd50ae9
Matijs van Zuijlen <matijs@matijs.net>
2009-10-24 16:02:08 +0200

    Activating the icons now opens the viewer.

--- lib/pinhole/mainwindow.rb
+++ lib/pinhole/mainwindow.rb
@@ -14,11 +14,18 @@ module Pinhole
       @window = @builder["mainwindow"]
 
       @browser = Pinhole::Browser.new
+      @box = @builder["mainvbox"]
 
       @browser.set_action do |iv, path|
-	p iv.model.get_iter(path).get_value(0)
+	filename = iv.model.get_iter(path).get_value(0)
+	@scrolledwindow.load_image_from_file(filename)
+	@box.remove @browser
+	@box.pack_start @scrolledwindow
+	@scrolledwindow.zoom_fit
+	@scrolledwindow.show_all
       end
-      @builder["mainvbox"].pack_start(@browser)
+
+      @box.pack_start(@browser)
 
       @store = Gtk::ListStore.new(String, Gdk::Pixbuf)
 
@@ -40,6 +47,7 @@ module Pinhole
       @builder = Gtk::Builder.new
       @builder.add Pinhole.path "data", "pinhole.xml"
       @builder.connect_signals { |name| method(name) }
+      @scrolledwindow = Image.new
     end
 
     def on_mainwindow_destroy
@@ -47,21 +55,41 @@ module Pinhole
     end
 
     def on_mainwindow_window_state_event w, e
+      if e.new_window_state.fullscreen?
+	@builder["menubar"].visible = false
+	@builder["statusbar"].visible = false
+	@scrolledwindow.fullscreen
+	@fullscreen = true
+      else
+	@builder["menubar"].visible = true
+	@builder["statusbar"].visible = true
+	@scrolledwindow.unfullscreen
+	@fullscreen = false
+      end
     end
 
     def on_menu_fullscreen_activate
+      if @fullscreen then
+	@window.unfullscreen
+      else
+	@window.fullscreen
+      end
     end
 
     def on_menu_zoom_in_activate
+      @scrolledwindow.zoom_in
     end
 
     def on_menu_zoom_out_activate
+      @scrolledwindow.zoom_out
     end
 
     def on_menu_zoom_fit_activate
+      @scrolledwindow.zoom_fit
     end
 
     def on_menu_zoom_100_activate
+      @scrolledwindow.zoom_100
     end
   end
 end
