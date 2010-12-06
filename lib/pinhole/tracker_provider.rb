5f9f0a863258159022e1147b0235fc30c8e6b5f1
Matijs van Zuijlen <matijs@matijs.net>
2010-12-06 22:38:02 +0100

    Use new Tracker interface with SPARQL.

--- lib/pinhole/tracker_provider.rb
+++ lib/pinhole/tracker_provider.rb
@@ -6,31 +6,43 @@ module Pinhole
 
     def initialize
       @session_bus = DBus::SessionBus.instance
-      @tracker = @session_bus.service("org.freedesktop.Tracker")
+      @tracker = @session_bus.service("org.freedesktop.Tracker1")
     end
 
     def each
       # Returns an array of arrays of arrays. The first element of each
       # sub-array is the file name.
-      list = searcher.Query(1, "Images", ["File:Mime"], "", [], "", false,
-			     [], false, 0, 50)
-      list[0].each {|e| yield e[0]}
+      #list = searcher.Query(1, "Images", ["File:Mime", "Image:CameraMake"],
+      #			    "", [], "", false, [], false, 0, 200)
+      list = searcher.SparqlQuery "
+      SELECT ?url ?typ
+      WHERE {
+	      ?photo a nmm:Photo ;
+		      nie:isStoredAs ?as .
+	      ?as nie:url ?url .
+	      ?photo nie:mimeType ?typ .
+      }
+      LIMIT 10
+      "
+
+      list[0].each {|e|
+	yield e[0].gsub(/file:\/\//, '') # if e[3] != ""
+      }
     end
 
     private
 
     def searcher
-      @searcher ||= get_iface("Search")
+      @searcher ||= get_iface("Resources")
     end
 
     def get_iface(name)
-      o = @tracker.object("/org/freedesktop/Tracker/#{name}")
+      o = @tracker.object("/org/freedesktop/Tracker1/#{name}")
       o.introspect
-      if o.has_iface? "org.freedesktop.Tracker.#{name}"
-	return o["org.freedesktop.Tracker.#{name}"]
+      if o.has_iface? "org.freedesktop.Tracker1.#{name}"
+	return o["org.freedesktop.Tracker1.#{name}"]
       else
-	puts "We have no #{name} interface"
-	return nil
+	raise "We have no #{name} interface"
       end
     end
   end
