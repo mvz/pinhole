da39c4d3ebc40683a8b03c540cec8291df716f44
Matijs van Zuijlen <matijs@matijs.net>
2020-09-26 23:22:15 +0200

    Replace ruby-dbus with gir_ffi-tracker
    The query seems to time out when using dbus. Also, gir_ffi-tracker uses
    Tracker's SPARQL client library directly.

--- lib/pinhole/tracker_provider.rb
+++ lib/pinhole/tracker_provider.rb
@@ -1,51 +1,35 @@
 # frozen_string_literal: true
 
-require "dbus"
+require "gir_ffi-tracker"
 require "cgi"
 
 module Pinhole
-  # Image provider using to the Tracker file searcher as its backend
+  # Image provider using the Tracker file searcher as its backend
   class TrackerProvider
     include Enumerable
 
-    def initialize
-      @session_bus = DBus::SessionBus.instance
-      @tracker = @session_bus.service("org.freedesktop.Tracker1")
-    end
-
     def each
-      # Returns an array of arrays of arrays. The first element of each
-      # sub-array is the file name.
-      list = searcher.SparqlQuery "
-      SELECT ?url ?typ
-      WHERE {
-              ?photo a nmm:Photo ;
-                      nie:isStoredAs ?as .
-              ?as nie:url ?url .
-              ?photo nie:mimeType ?typ .
-      }
-      LIMIT 10
-      "
+      cursor = searcher.query <<~SPARQL
+        SELECT ?url ?typ
+        WHERE {
+                ?photo a nmm:Photo ;
+                        nie:isStoredAs ?as .
+                ?as nie:url ?url .
+                ?photo nie:mimeType ?typ .
+        }
+        LIMIT 10
+      SPARQL
 
-      list[0].each do |e|
-        yield CGI.unescape(e[0].gsub(%r{file://}, "")) # if e[3] != ""
+      while cursor.next
+        uri = cursor.get_string 0
+        yield CGI.unescape(uri.gsub(%r{file://}, ""))
       end
     end
 
     private
 
     def searcher
-      @searcher ||= get_iface("Resources")
-    end
-
-    def get_iface(name)
-      o = @tracker.object("/org/freedesktop/Tracker1/#{name}")
-      o.introspect
-      unless o.has_iface? "org.freedesktop.Tracker1.#{name}"
-        raise "We have no #{name} interface"
-      end
-
-      o["org.freedesktop.Tracker1.#{name}"]
+      @searcher ||= Tracker::SparqlConnection.get
     end
   end
 end
